5a4030799140a36d5988d3b73425e7f606876ff5
unknown <sripranathi.indupalli.s72@kalvium.community>
2025-04-21 10:43:03 +0530

    Add auth endpoints with JWT and cookie support

--- Backend/server.js
+++ Backend/server.js
@@ -2,11 +2,14 @@ require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
-const cityRoutes = require("./routes");
+const cityRoutes = require("./routes/routes");
+const cookieParser = require('cookie-parser');
+const authRoutes = require('./routes/auth.routes');
 
 const app = express();
 app.use(express.json());
 app.use(cors()); 
+app.use(cookieParser());
 const PORT = process.env.PORT || 3000;
 
 mongoose.connect(process.env.MONGO_URI, {
@@ -17,6 +20,7 @@ mongoose.connect(process.env.MONGO_URI, {
 .catch(err => console.error("❌ MongoDB connection error:", err));
 
 app.use("/", cityRoutes);
+app.use('/api/auth', authRoutes);
 
 app.listen(PORT, () => {
   console.log(`🚀 Server running on http://localhost:${PORT}`);
